71f40edd494a6d8bc9a766d1b89dbba304779bb2
moqian <zxj198468@163.com>
2026-08-20 17:11:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import os
 import sys
+import time
 
 print("hello moqian!")
+
+# 可选：短暂暂停，让窗口停留
+
+time.sleep(5)  # 等待 2 秒后自动关闭
 
 # 通过环境变量控制是否等待输入
 wait = os.getenv('WAIT_ON_EXIT', 'false').lower() == 'true'
